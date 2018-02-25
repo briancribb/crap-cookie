d3e5cc9a6b465a7f8bba9b3d4162be25331cb48d
Brian Cribb <brian@briancribb.com>
2018-02-25 09:52:24 -0500

    Setting up.

---
+++ crap.js
@@ -0,0 +1,13 @@
+var crapCookie = (function(){
+	// Stuff to be kept in the clusure.
+
+
+
+	// Object to return.
+	return {
+		create: function(){},
+		read: function(){},
+		adjust: function(){},
+		purge: function(){},
+	};
+}());
